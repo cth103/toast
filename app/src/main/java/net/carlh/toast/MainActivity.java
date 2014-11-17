047e58b0a00c3dbf2495e165bc7725dc831fde7c
Carl Hetherington <cth@carlh.net>
2014-11-17 00:03:20 +0000

    GPL boilerplate.

--- app/src/main/java/net/carlh/toast/MainActivity.java
+++ app/src/main/java/net/carlh/toast/MainActivity.java
@@ -1,3 +1,22 @@
+/*
+    Copyright (C) 2014 Carl Hetherington <cth@carlh.net>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+*/
+
 package net.carlh.toast;
 
 import android.content.Intent;
@@ -19,6 +38,10 @@ public class MainActivity extends ActionBarActivity {
     */
     private static State state;
 
+    static State getState() {
+        return state;
+    }
+
     private TextView temperature;
     private TextView target;
     private ToggleButton enabled;
@@ -26,10 +49,6 @@ public class MainActivity extends ActionBarActivity {
     private Button colder;
     private TextView on;
     private TextView explanation;
-
-    static State getState() {
-        return state;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
