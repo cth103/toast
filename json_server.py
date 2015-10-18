157c1f01815c2e770ad89f342e73aba7121c799b
Carl Hetherington <cth@carlh.net>
2015-10-18 01:05:59 +0100

    Initial hacks.

---
+++ json_server.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+#
+#    Copyright (C) 2014-2015 Carl Hetherington <cth@carlh.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+import threading
+import socket
+
+import util
+
+class Server:
+    def __init__(self, port):
+        # Port number to listen on
+        self.port = port
+        # Active client sockets
+        self.clients = []
+        # Mutex for self.clients
+        self.mutex = threading.Lock()
+
+    # Start the server
+    def start(self):
+        for res in socket.getaddrinfo(None, self.port, socket.AF_UNSPEC, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
+            af, socktype, proto, canonname, sa = res
+            try:
+                s = socket.socket(af, socktype, proto)
+            except socket.error as msg:
+                s = None
+                continue
+
+            try:
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                s.bind(sa)
+                s.listen(1)
+            except socket.error as msg:
+                s.close()
+                s = None
+                continue
+
+            break
+
+        if s is None:
+            raise util.Error('could not open socket (%s)' % msg)
+
+        self.accept_thread = threading.Thread(target=self.run, args=(s,))
+        self.accept_thread.start()
+
+    # Thread which listens for incoming connections
+    def run(self, sock):
+        while True:
+            conn, addr = sock.accept()
+            conn.settimeout(10)
+            threading.Thread(target=self.client, args=(conn,)).start()
+
+    # Thread to handle one client session
+    def client(self, conn):
+        with self.mutex:
+            self.clients.append(conn)
+
+        try:
+            while True:
+                json = util.receive_json(conn)
+                self.handler(json)
+        except:
+            pass
+
+        with self.mutex:
+            self.clients.remove(conn)
+
+        conn.close()
+
+    def send(self, d):
+        with self.mutex:
+            for c in self.clients:
+                util.send_json(c, d)
---
+++ util.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+#
+#    Copyright (C) 2014-2015 Carl Hetherington <cth@carlh.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program; if not, write to the Free Software
+#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+import json
+
+class Error(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return self.value
+    def __repr__(self):
+        return str(self)
+
+def warning(w):
+    print 'WARNING: %s' % w
+
+def verbose(v):
+    print v
+
+def send_json(socket, data):
+    """Send a dict as JSON to a socket"""
+    s = json.dumps(data)
+    length = bytearray(4)
+    length[0] = (len(s) >> 24) & 0xff
+    length[1] = (len(s) >> 16) & 0xff
+    length[2] = (len(s) >>  8) & 0xff
+    length[3] = (len(s) >>  0) & 0xff
+    socket.sendall(length)
+    socket.sendall(s)
+    verbose('-> %s' % data)
+
+def get_data(sock, length):
+    """Get some data from a socket"""
+    all = ""
+    got = 0
+    while got < length:
+        d = sock.recv(length - got)
+        if not d:
+            break
+        all += d
+        got += len(d)
+
+    return all
+
+def receive_json(socket):
+    """Receive some JSON from a socket"""
+    s = get_data(socket, 4)
+    if len(s) < 4:
+        raise Error('could not get data from socket (got %d instead of 4)' % len(s))
+
+    size = (ord(s[0]) << 24) | (ord(s[1]) << 16) | (ord(s[2]) << 8) | ord(s[3])
+    s = get_data(socket, size)
+    if len(s) != size:
+        raise Error('could not get data from socket (got %d instead of %d)' % (len(s), size))
+
+    verbose('<- %s' % s)
+    return json.loads(s);
