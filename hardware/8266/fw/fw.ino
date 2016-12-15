0a491e16b1822be835dbedb6e915908e0612ac39
Carl Hetherington <cth@carlh.net>
2016-12-15 01:34:34 +0000

    More hacks related to 1wb.

--- hardware/8266/fw/fw.ino
+++ hardware/8266/fw/fw.ino
@@ -1,6 +1,8 @@
 /* -*- c-basic-offset: 2; tab-width: 2 -*-; indent-tabs-mode: nil; */
 
 #include <SoftwareSerial.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define SSID "PlusnetWireless998FC7"
 #define PASS "E5383F2817"
@@ -10,9 +12,15 @@
 #define ESP8266_RX_PIN 2
 #define DEBUG_TX_PIN 3
 #define DEBUG_RX_PIN 4
+#define ONE_WIRE_BUS 4
 
 SoftwareSerial wifi(ESP8266_RX_PIN, ESP8266_TX_PIN);
 SoftwareSerial debug(DEBUG_RX_PIN, DEBUG_TX_PIN);
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensor(&oneWire);
+
+uint8_t scratchPad[9];
+uint8_t const sensorAddress[8]= { 0x28, 0xee, 0xe1, 0xe2, 0x12, 0x16, 0x1, 0x60 };
 
 void
 resetWifi()
@@ -32,6 +40,7 @@ setup()
   pinMode(ESP8266_CH_PD_PIN, OUTPUT);
   pinMode(ESP8266_RX_PIN, INPUT);
   pinMode(DEBUG_TX_PIN, OUTPUT);
+  pinMode(ONE_WIRE_BUS, OUTPUT);
 
   OSCCAL = 82;
 
@@ -39,56 +48,58 @@ setup()
   debug.println("Hello world!");
   debug.flush();
 
+  return;
+
   wifi.begin(9600);
   wifi.listen();
 
-  debug.println("Resetting ESP8266.");
+  //debug.println("Resetting ESP8266.");
   resetWifi();
   delay(2000);
 
   wifi.setTimeout(5000);
   if (wifi.find("WIFI GOT IP")) {
-    debug.println("ESP8266 already connected.");
-    debug.flush();
+    //debug.println("ESP8266 already connected.");
+    //debug.flush();
     return;
   }
 
   while (true) {
 
     wifi.setTimeout(5000);
-    debug.println("Talking to ESP8266.");
-    debug.flush();
+    //debug.println("Talking to ESP8266.");
+    //debug.flush();
     wifi.print("AT+CWMODE=1\r\n");
     wifi.flush();
     if (!wifi.find("OK")) {
-      debug.println("failed.");
-      debug.flush();
+      //debug.println("failed.");
+      //debug.flush();
       continue;
     }
 
     wifi.setTimeout(25000);
-    debug.println("ok");
-    debug.println("Connecting to network.");
-    debug.flush();
+    //debug.println("ok");
+    //debug.println("Connecting to network.");
+    //debug.flush();
     wifi.print("AT+CWJAP=\"" SSID "\",\"" PASS "\"\r\n");
     wifi.flush();
     if (!wifi.find("OK")) {
-      debug.println("failed.");
+      //debug.println("failed.");
       continue;
     }
 
     wifi.setTimeout(1000);
-    debug.println("ok.");
-    debug.println("Enabling DHCP.");
-    debug.flush();
+    //debug.println("ok.");
+    //debug.println("Enabling DHCP.");
+    //debug.flush();
     wifi.print("AT+CWDHCP=1,1\r\n");
     wifi.flush();
     if (!wifi.find("OK")) {
-      debug.println("failed.");
+      //debug.println("failed.");
       continue;
     }
 
-    debug.println("ok.");
+    //debug.println("ok.");
     break;
   }
 }
@@ -96,7 +107,9 @@ setup()
 void
 loop()
 {
-  wifi.print(F("AT+CIPSTART=\"TCP\",\"192.168.1.1\",4024\r\n"));
+  debug.println(sensor.getTempC(sensorAddress), 2);
+
+  wifi.print("AT+CIPSTART=\"TCP\",\"192.168.1.1\",4024\r\n");
   wifi.find("OK");
 
   wifi.print("AT+CIPSEND=14\r\n");
