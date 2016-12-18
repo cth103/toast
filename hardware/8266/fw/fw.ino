79dc10226a552b755e27c271ea2646515080598e
Carl Hetherington <cth@carlh.net>
2016-12-18 16:26:48 +0000

    Various work on schematic/diagrams and code.

--- hardware/8266/fw/fw.ino
+++ hardware/8266/fw/fw.ino
@@ -7,21 +7,21 @@
 #define SSID "PlusnetWireless998FC7"
 #define PASS "E5383F2817"
 
-#define ESP8266_TX_PIN 0
+#define ESP8266_TX_PIN 2
 #define ESP8266_CH_PD_PIN 1
-#define ESP8266_RX_PIN 2
-#define DEBUG_TX_PIN 3
-#define DEBUG_RX_PIN 4
+#define ESP8266_RX_PIN 0
+#define RELAY 3
 #define ONE_WIRE_BUS 4
+#define LISTEN_IP "192.168.1.7"
+#define LISTEN_PORT "4024"
 
 SoftwareSerial wifi(ESP8266_RX_PIN, ESP8266_TX_PIN);
-//SoftwareSerial debug(DEBUG_RX_PIN, DEBUG_TX_PIN);
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensor(&oneWire);
 
 uint8_t scratchPad[9];
 uint8_t const sensorAddress[8] = { 0x28, 0xee, 0xe1, 0xe2, 0x12, 0x16, 0x1, 0x60 };
-char* connect[] = { "AT+CWMODE=1", "AT+CWJAP=\"" SSID "\",\"" PASS "\"", "AT+CWDHCP=1,1" };
+char* connect[] = { "CWMODE=1", "CWJAP=\"" SSID "\",\"" PASS "\"", "CIPSTA=\"" LISTEN_IP "\"" };
 
 void
 resetWifi()
@@ -31,15 +31,22 @@ resetWifi()
   digitalWrite(ESP8266_CH_PD_PIN, LOW);
   delay(1000);
   digitalWrite(ESP8266_CH_PD_PIN, HIGH);
-  //wifi.print("AT+RST\r\n");
 }
 
 bool
 sendWithOk(char const * message)
 {
+  wifi.print("AT+");
   wifi.print(message);
   wifi.print("\r\n");
   return wifi.find("OK");
+}
+
+void
+startServer()
+{
+  sendWithOk("CIPMUX=1");
+  sendWithOk("CIPSERVER=1," LISTEN_PORT);
 }
 
 void
@@ -48,14 +55,10 @@ setup()
   pinMode(ESP8266_TX_PIN, OUTPUT);
   pinMode(ESP8266_CH_PD_PIN, OUTPUT);
   pinMode(ESP8266_RX_PIN, INPUT);
-  //pinMode(DEBUG_TX_PIN, OUTPUT);
   pinMode(ONE_WIRE_BUS, OUTPUT);
+  pinMode(RELAY, OUTPUT);
 
   OSCCAL = 82;
-
-  //debug.begin(9600);
-  //debug.println("Hello world!");
-  //debug.flush();
 
   wifi.begin(9600);
   wifi.listen();
@@ -64,13 +67,8 @@ setup()
   delay(2000);
 
   wifi.setTimeout(5000);
-  if (wifi.find("GOT IP")) {
-    return;
-  }
 
   while (true) {
- 
-    wifi.setTimeout(5000);
     int i;
     for (i = 0; i < 3; ++i) {
       if (!sendWithOk(connect[i])) {
@@ -79,6 +77,7 @@ setup()
     }
 
     if (i == 3) {
+      startServer();
       return;
     }
   }
@@ -87,13 +86,24 @@ setup()
 void
 loop()
 {
-  sendWithOk("AT+CIPMUX=1");
-  sendWithOk("AT+CIPSERVER=1");
-  while (!wifi.find("S")) {}
-  sendWithOk("AT+CIPSEND=0,7");
-  wifi.find(">");
-  sensor.requestTemperatures();
-  wifi.print(sensor.getTempC(sensorAddress), 2);
-  sendWithOk("");
-  sendWithOk("AT+CIPCLOSE=0");
+  while (true) {
+    char c = wifi.read();
+    if (c == 's') {
+      sendWithOk("CIPSEND=0,7"); 
+      wifi.find(">");
+      sensor.requestTemperatures();
+      wifi.println(sensor.getTempC(sensorAddress), 2);
+      wifi.find("OK");
+      break;
+    } else if (c == 'p') {
+      digitalWrite(RELAY, true);
+      break;
+    } else if (c == 'q') {
+      digitalWrite(RELAY, false);
+      break;
+    }
+  }
+
+  sendWithOk("CIPCLOSE=0");
 }
+
