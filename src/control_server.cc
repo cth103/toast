a719d159959811fd72fe647b1641d129a3e2f03d
Carl Hetherington <cth@carlh.net>
2018-10-03 10:52:35 +0100

    Fix protocols.

--- src/control_server.cc
+++ src/control_server.cc
@@ -2,10 +2,12 @@
 #include "state.h"
 #include "toast_socket.h"
 #include "types.h"
+#include "log.h"
 
 using std::list;
 using std::string;
 using std::shared_ptr;
+using std::runtime_error;
 
 ControlServer::ControlServer(State* state, int port, int timeout)
 	: Server(port, timeout)
@@ -31,43 +33,63 @@ ControlServer::zones_from_message(uint8_t*& p)
 }
 
 void
+ControlServer::write(shared_ptr<Socket> socket, uint8_t const * data, int length)
+{
+	uint8_t len[4];
+	len[0] = (length & 0xff000000) >> 24;
+	len[1] = (length & 0x00ff0000) >> 16;
+	len[2] = (length & 0x0000ff00) >> 8;
+	len[3] = (length & 0x000000ff) >> 0;
+	socket->write(len, 4);
+	socket->write(data, length);
+}
+
+void
 ControlServer::handle(shared_ptr<Socket> socket)
+try
 {
 	uint8_t len[4];
 	if (socket->read(len, 4) != 4) {
 		return;
 	}
 
+
 	uint32_t length = (len[0] << 24) | (len[1] << 16) | (len[2] << 8) | len[3];
+	LOG("Client sending %1 bytes", length);
 	shared_ptr<uint8_t[]> data(new uint8_t[length]);
 	if (socket->read(data.get(), length) != static_cast<int>(length)) {
 		return;
 	}
 
 	uint8_t* p = data.get();
+	LOG("Opcode is %1", static_cast<int>(p[0]));
 	if (p[0] == OP_SEND_BASIC) {
 		auto s = _state->get(false, OP_ALL);
-		socket->write(s.first.get(), s.second);
+		write(socket, s.first.get(), s.second);
 	} else if (p[0] == OP_SEND_ALL) {
 		auto s = _state->get(true, OP_ALL);
-		socket->write(s.first.get(), s.second);
+		write(socket, s.first.get(), s.second);
 	} else if (p[0] == OP_HEATING_ENABLED) {
 		_state->set_heating_enabled(p[1]);
 		auto s = _state->get(false, OP_HEATING_ENABLED);
-		socket->write(s.first.get(), s.second);
+		write(socket, s.first.get(), s.second);
 	} else if (p[0] == OP_ZONE_HEATING_ENABLED) {
 		p++;
 		for (auto i: zones_from_message(p)) {
 			_state->set_zone_heating_enabled(i, *p++);
 		}
 		auto s = _state->get(false, OP_ZONE_HEATING_ENABLED);
-		socket->write(s.first.get(), s.second);
+		write(socket, s.first.get(), s.second);
 	} else if (p[0] == OP_TARGET) {
 		p++;
 		for (auto i: zones_from_message(p)) {
 			_state->set_target(i, static_cast<float>(p[0] | (p[1] << 8)) / 16);
 		}
 		auto s = _state->get(false, OP_TARGET);
-		socket->write(s.first.get(), s.second);
+		write(socket, s.first.get(), s.second);
 	}
+}
+catch (runtime_error& e)
+{
+	LOG("ControlServer conversion died: %1", e.what());
 }
--- src/control_server.h
+++ src/control_server.h
@@ -12,5 +12,7 @@ private:
 	void handle(std::shared_ptr<Socket> socket);
 	std::list<std::string> zones_from_message(uint8_t*& p);
 
+	static void write(std::shared_ptr<Socket>, uint8_t const *, int);
+
 	State* _state;
 };
