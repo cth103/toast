4b23a849479efab41155394c6ecb70100b120c96
Carl Hetherington <cth@carlh.net>
2018-10-11 10:12:19 +0100

    Fix year in logs.

--- src/log.cc
+++ src/log.cc
@@ -27,6 +27,6 @@ Log::log(int type, string s)
 	time_t t = time(0);
 	struct tm tm = *localtime(&t);
 
-	fprintf(f, "%02d/%02d/%02d %02d:%02d:%02d %s\n", tm.tm_mday, tm.tm_mon, tm.tm_year, tm.tm_hour, tm.tm_min, tm.tm_sec, s.c_str());
+	fprintf(f, "%02d/%02d/%02d %02d:%02d:%02d %s\n", tm.tm_mday, tm.tm_mon, tm.tm_year + 1900, tm.tm_hour, tm.tm_min, tm.tm_sec, s.c_str());
 	fclose(f);
 }
